c83d4ec603adfcc2ea1fa0b7a603f246ee679729
Bishal Dofo <dofobishal@gmail.com>
2024-01-18 22:32:31 +0600

    Navbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
-        {children}
+        <div className='max-w-7xl m-auto'>
+          <Navbar></Navbar>
+          {children}
+        </div>
       </body>
     </html>
   )
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,29 +1,30 @@
-'use client'
+"use client"
+
+import Link from "next/link";
+import { IoHome } from "react-icons/io5";
 
 const Navbar = () => {
    return (
-      <div>
-         <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li>
-        <details>
-          <summary>
-            Parent
-          </summary>
-          <ul className="p-2 bg-base-100 rounded-t-none">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
-          </ul>
-        </details>
-      </li>
-    </ul>
-  </div>
-</div>
+      <div className="navbar bg-base-100">
+         <div className="navbar-start">
+            <div className="dropdown">
+               <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+               </div>
+               <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                  <li><Link href="/"><IoHome />Home</Link></li>
+               </ul>
+            </div>
+            <a className="btn btn-ghost text-xl text-[#0079D3]">Dialogify</a>
+         </div>
+         <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+               <li><Link href="/"><IoHome className="text-3xl" /></Link></li>
+            </ul>
+         </div>
+         <div className="navbar-end">
+            <a className="btn">Button</a>
+         </div>
       </div>
    );
 };
