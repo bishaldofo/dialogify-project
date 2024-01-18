803e336120e9d7dc2f3437e9d4996ac595db3b84
Bishal Dofo <dofobishal@gmail.com>
2024-01-18 22:57:48 +0600

    Home Navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
                </div>
                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                   <li><Link href="/"><IoHome />Home</Link></li>
+                  <li><Link href="/"><IoHome />About</Link></li>
                </ul>
             </div>
             <a className="btn btn-ghost text-xl text-[#0079D3]">Dialogify</a>
