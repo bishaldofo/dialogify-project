200f86f8548e119b4e94d59fdc7120bba2c3fd27
Bishal Dofo <dofobishal@gmail.com>
2024-01-18 23:16:08 +0600

    Home nav

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -12,16 +12,18 @@ const Navbar = () => {
                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                </div>
                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                  <li><Link href="/"><IoHome />Home</Link></li>
-                  <li><Link href="/"><IoHome />About</Link></li>
-                  <li><Link href="/"><IoHome />Contact</Link></li>
+                  <li><IoHome /><Link href="/">Home</Link></li>
+                  <li><Link href="/">About</Link></li>
+                  <li><Link href="/">Contact</Link></li>
                </ul>
             </div>
             <a className="btn btn-ghost text-xl text-[#0079D3]">Dialogify</a>
          </div>
          <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
-               <li><Link href="/"><IoHome className="text-3xl" /></Link></li>
+               <li><Link href="/"><IoHome className="text-3xl" />Home</Link></li>
+               <li><Link href="/">About</Link></li>
+               <li><Link href="/">Contact</Link></li>
             </ul>
          </div>
          <div className="navbar-end">
