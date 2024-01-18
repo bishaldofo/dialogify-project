84031a2733d2e9f9edbbf03ae653850bf4677453
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-19 01:02:53 +0600

    homepage layout setup with sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
 const HomePage = () => {
   return (
-      <div>
-      </div>
+      <section>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+      <div className="bg-green-300 h-[500px]">
+        <h2>Left side</h2>
+       </div>
+       <div className="md:col-span-2 ">
+         <div className="">
+          Main contents are comes here
+         </div>
+       </div>
+       <div className="bg-orange-200 h-[500px]">
+          <h2>Right Side</h2>
+       </div>
+         </div>
+      </section>
   )
 }
 
