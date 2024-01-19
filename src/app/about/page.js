d0b56e6ee9519faeb33bf73178f8579e3990a95d
Bishal Dofo <dofobishal@gmail.com>
2024-01-19 20:09:14 +0600

    Home page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,54 +1,58 @@
-import Image from "next/image";
+const { default: Image } = require("next/image");
 
-
-const AboutPage = () => {
+const page = () => {
     return (
-        <div className="w-screen">
-            <div className="bg-[#FF4500] h-screen">
-                <div className="text-center flex items-center justify-center h-screen max-w-sm md:max-w-lg mx-auto">
-                    <div className="space-y-4">
-                        <h1 className="text-5xl text-white font-bold">Community Building</h1>
-                        <p className="text-[20px] text-gray-700">Join us in building a community where your voice matters. Whether you are here to share your expertise, learn from others, or simply connect with like-minded individuals, Dilogify is your platform to engage and grow.</p>
-
+        <div className="">
+            <div className="bg-[#FF4500] flex flex-col md:flex-row gap-5 items-center justify-center py-20">
+                <div className="max-w-3xl flex gap-6 items-center">
+                    <div className="w-1/2">
                         <Image src="https://i.ibb.co/FnT729k/community.jpg" alt="community image"
                             height={600}
                             width={600}
                         />
                     </div>
+                    <div className="w-1/2 space-y-4">
+                        <h1 className="text-3xl text-white font-bold">Community Building</h1>
+                        <p className="text-base text-white">Join us in building a community where your voice matters. Whether you are here to share your expertise, learn from others, or simply connect with like-minded individuals, Dilogify is your platform to engage and grow.</p>
+
+                    </div>
                 </div>
             </div>
 
-            <div className=" h-screen">
-                <div className="text-center flex items-center justify-center h-screen max-w-sm md:max-w-lg mx-auto">
-                    <div className="space-y-4">
-                        <h1 className="text-5xl font-bold">Our Mission</h1>
-                        <p className="text-[20px] text-gray-700">Our mission is to provide a space for open and respectful dialogue. We strive to create an inclusive environment
-                            where users can express themselves, learn from others, and engage in conversations that matter.</p>
+            <div className="flex gap-5 flex-col md:flex-row items-center justify-center py-20">
+                <div className="max-w-3xl flex gap-6 items-center">
+                    <div className="w-1/2 space-y-4">
+                        <h1 className="text-3xl text-right text-black font-bold">Our Mission</h1>
+                        <p className="text-base text-right text-black">Our mission is to provide a space for open and respectful dialogue. We strive to create an inclusive environment
+                        where users can express themselves, learn from others, and engage in conversations that matter.</p>
 
+                    </div>
+                    <div className="w-1/2">
                         <Image src="https://i.ibb.co/XFk3fXH/mission.png" alt="community image"
                             height={600}
                             width={600}
                         />
                     </div>
                 </div>
-
             </div>
-            <div className=" h-screen bg-[#FF4500]">
-                <div className="text-center flex items-center justify-center h-screen max-w-sm md:max-w-lg mx-auto">
-                    <div className="space-y-4">
-                        <h1 className="text-5xl font-bold">Privacy and Security</h1>
-                        <p className="text-[20px] text-gray-700">Your privacy and security are our top priorities. We implement robust measures to safeguard your data, ensuring you can freely express yourself without concerns.</p>
 
+            <div className="bg-[#FF4500] flex flex-col md:flex-row gap-5 items-center justify-center py-20">
+                <div className="max-w-3xl flex gap-6 items-center">
+                    <div className="w-1/2">
                         <Image src="https://i.ibb.co/R055qmB/security.jpg" alt="community image"
                             height={600}
                             width={600}
                         />
                     </div>
-                </div>
+                    <div className="w-1/2 space-y-4">
+                        <h1 className="text-3xl text-white font-bold">Privacy and Security</h1>
+                        <p className="text-base text-white">Your privacy and security are our top priorities. We implement robust measures to safeguard your data, ensuring you can freely express yourself without concerns.</p>
 
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
 
-export default AboutPage;
+export default page;
