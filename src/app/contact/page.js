676bbb5069d43333d10c190ead4e96253f79cd2b
Bishal Dofo <dofobishal@gmail.com>
2024-01-21 01:44:34 +0600

    Updated

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,12 +1,5 @@
 
 
-<<<<<<< HEAD
-                  <button class='py-2 px-5 rounded-2xl bg-[#FF4500] text-white border-none  transition duration-700 ease-in hover:bg-[#140904]'>
-                     Contact Us
-                  </button>
-               </div>
-           </div>
-=======
 import React from 'react';
 import contactImage from '../../assets/Our Team.png'
 import Image from 'next/image';
@@ -17,17 +10,15 @@ const contactpage = () => {
         <div>
             <div className=' relative ' >
             
-            <Image src={contactImage}  alt='' />
-            <div className=' absolute top-[20%] left-[10%] space-y-6 ' >
-            <h1 className=' text-5xl text-white font-semibold ' >Reach Out to Our Team</h1>
-<p className=' text-xl text-white font-semibold ' >Have questions or suggestions? <br/> Contact us anytime – our team is here to assist you!</p>
->>>>>>> 006652d7e4142f3e20c8fc35feea00faa7acc5d5
+                <Image src={contactImage}  alt='' />
+                <div className=' absolute top-[20%] left-[10%] space-y-6 ' >
+                    <h1 className=' text-5xl text-white font-semibold ' >Reach Out to Our Team</h1>
+                    <p className=' text-xl text-white font-semibold ' >Have questions or suggestions? <br/> Contact us anytime – our team is here to assist you!</p>
 
-<button className='py-2 px-5 rounded-2xl bg-[#FF4500] text-white border-none  transition duration-700 ease-in hover:bg-[#140904] '>
-  Contact Us
-</button>
-
-            </div>
+                    <button className='py-2 px-5 rounded-2xl bg-[#FF4500] text-white border-none  transition duration-700 ease-in hover:bg-[#140904] '>
+                    Contact Us
+                    </button>
+                </div>
             </div>
 
             <div className=' flex justify-center gap-40 ' >
