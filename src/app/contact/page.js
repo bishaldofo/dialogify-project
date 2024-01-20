8b1f35339301e8297a5c64e88b6c19ef21c224a3
Bishal Dofo <dofobishal@gmail.com>
2024-01-21 01:41:21 +0600

    Updated

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -10,10 +10,9 @@ const page = () => {
                   <h1 className=' text-3xl text-white font-semibold ' >Reach Out to Our Team</h1>
                   <p className=' text-base text-white font-semibold ' >Have questions or suggestions? <br/> Contact us anytime – our team is here to assist you!</p>
 
-                  <button class='py-2 px-5 rounded-2xl bg-[#FF4500] text-white border-none  transition duration-700 ease-in hover:bg-[#140904] '>
+                  <button class='py-2 px-5 rounded-2xl bg-[#FF4500] text-white border-none  transition duration-700 ease-in hover:bg-[#140904]'>
                      Contact Us
                   </button>
-
                </div>
            </div>
 
