3a55108f84875baccfcdf3bcba0585ba1788f0f2
Bishal Dofo <dofobishal@gmail.com>
2024-01-26 18:45:12 +0600

    updated

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -11,13 +11,16 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col md:flex-row mt-5 gap-5">
+      
       <div className="w-full md:w-[250px]">
         <Sidebar/>
       </div>
+      
       <div className="max-w-6xl m-auto flex-1 space-y-5">
         <PostCreate/>
         <Content/>
       </div>
+      
       <div className="w-full md:w-[250px]">
         <Advertise/>
       </div>
