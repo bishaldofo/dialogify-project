89936c796bb1660fd34372a53d37f85e47a5c8c6
Bishal Dofo <dofobishal@gmail.com>
2024-02-14 01:36:27 +0600

    Pull from stage

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,7 +1,7 @@
 const loading = () => {
    return (
       <div className="flex items-center h-screen justify-center">
-         <h1>Loading....</h1>
+         <h1>Loading.....</h1>
       </div>
    );
 };
